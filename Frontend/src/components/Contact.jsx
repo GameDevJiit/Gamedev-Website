d29010f232038fcfb8b170d6b1ba0f06e877c444
Priyansh Singh Tanwar <154755072+Priyansh6747@users.noreply.github.com>
2025-07-13 14:01:22 +0530

    minor styling change

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -9,7 +9,7 @@ const ImageClipBox = ({ src, clipClass }) => (
 
 const Contact = () => {
   return (
-      <div id="contact" className="my-20 min-h-96 w-screen  px-10">
+      <div id="contact" className="my-20 min-h-96 w-screen  px-10 md:max-h-52">
         <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
           <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
             <ImageClipBox
@@ -44,7 +44,7 @@ const Contact = () => {
             />
 
             <div className="mb-6 mt-8">
-              <p className="mb-2 text-sm text-blue-200">Get in touch with us:</p>
+              <p className="mb-2 text-sm text-amber-50">Get in touch with us:</p>
               <p className="text-lg font-semibold text-blue-300">
                 multimedia.development.jiit@gmail.com <br/>
                 shantanugupta.cy80@gmail.com
